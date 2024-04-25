5b5ed2ed2ee54bd6c65b30aad5aafc0c88fa1e49
Mbah Neilvani <neilvanimbah@gmail.com>
2024-04-25 10:05:59 +0100

    added

--- Js/string repeat.js
+++ Js/string repeat.js
@@ -3,3 +3,11 @@
 function stringRepeat(integer, string){
     return s.repeat(n);  
 }
+
+
+function repeatStr (n, s) {
+    var str="";
+    for(var i=0; i < n; i++)
+      str+=s;
+      return str;
+    }
